af1b3a965cb3bda6798cbd5b5bd7ac80411a65e7
Caristop3 <leecaristop3@gmail.com>
2016-12-06 13:20:24 +0800

    1206

--- NodeJS20161123/schemas/users.js
+++ NodeJS20161123/schemas/users.js
@@ -3,6 +3,10 @@ var mongoose = require('mongoose');
 
 //声明一个mongoons对象
 var UsersSchema = new mongoose.Schema({
+    //传入一个JSON对象，该对象形如 xxx:yyyy 
+    //yyy是一个Schema.Type，定义了属性类型
+    //Schema.Type是由Mongoose内定的一些数据类型，基本数据类型都在其中，他也内置了一些Mongoose特有的Schema.Type。
+    //当然，你也可以自定义Schema.Type，只有满足Schema.Type的类型才能定义在Schema内
     name:String,
     paw:String,
     meta:{
@@ -18,12 +22,14 @@ var UsersSchema = new mongoose.Schema({
 })
 
 //每次执行都会调用，时间更新操作
+//串行使用pre方法
 UsersSchemal.pre('save',function(next){
     if(this.isNew){
         this.meta.createAt = Date.now();
     }else{
         this.meta.updateAt = Date.now()
     }
+    //执行下一个方法使用next调用
     next();
 })
 
